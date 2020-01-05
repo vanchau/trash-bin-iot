f8d6e101c6a01c48a36cc34c6128db41c98a7972
vchau <van.chau@aalto.fi>
2020-01-05 19:04:09 +0200

    fix api key

--- frontend/src/components/GoogleMaps/index.js
+++ frontend/src/components/GoogleMaps/index.js
@@ -11,13 +11,13 @@ const locationOverflow = require('../../static/location-full.png')
 const locationOk = require('../../static/location-ok.png')
 const locationOverflowAndLate = require('../../static/location-late-pickup.png')
 
-const key = process.env.REACT_APP_GOOGLE_API_KEY != null ? process.env.REACT_APP_GOOGLE_API_KEY : '';
+const key = process.env.REACT_APP_GOOGLE_API_KEY !== null ? process.env.REACT_APP_GOOGLE_API_KEY : '';
 
 const getTrashBins = bins => bins.map(bin => ({ type: bin.wasteType, fillStatus: bin.fillStatus }))
 
 const GoogleMaps = compose(
   withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${ key }`, // add google maps api key to the end of the line.
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${key}`, // add google maps api key to the end of the line.
     loadingElement: <div style={ { height: '100%' } } />,
     containerElement: <div style={ { height: '100%' } } />,
     mapElement: <div style={ { height: '100%' } } />,
