ec71f9e76334ae3c70567dc41d91da28d88d6233
vchau <van.chau@aalto.fi>
2020-01-05 18:01:48 +0200

    move server.js

---
+++ frontend/server.js
@@ -0,0 +1,15 @@
+  const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
